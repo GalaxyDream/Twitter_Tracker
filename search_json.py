9e177420d3ce7808e4fe6502795d66f051cec623
Bian <bianjiang@fctr-3228-m2.local>
2016-02-17 23:55:54 -0500

    search query

---
+++ search_json.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import logging
+import logging.handlers
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG, format='(%(asctime)s) [%(process)d] %(levelname)s: %(message)s')
+requests_log = logging.getLogger("requests")
+requests_log.setLevel(logging.WARNING)
+
+import sys,json
+import hashlib
+def md5(data):
+    return hashlib.md5(data).hexdigest()
+
+
+search_terms = ['hpv', '#hpv', 'Human Papillomavirus Vaccines', 'Human Papillomavirus Vaccine',
+    'HumanPapillomavirusVaccine', '#HumanPapillomavirusVaccine'
+    ]
+def generate_search_json():
+
+    with open('search.json', 'w') as wf:
+        results = {}
+
+        querystring = '%s'%(' OR '.join('"' + term.lower() + '"' for term in search_terms))
+
+        output_filename = md5(querystring.encode('utf-8'))
+
+        
+        results[output_filename] = {
+            "terms": search_terms,
+            "since_id": 0,
+            "querystring": querystring,
+            "output_filename": output_filename
+        }
+
+        json.dump(results, wf)
+
+if __name__=="__main__":
+
+    generate_search_json()
