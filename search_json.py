e71b428031cbde0f6ed2077de07e9442cbdfc2cc
Jiang Bian <bianjiang@Jiangs-iMac.local>
2016-09-05 09:50:02 -0400

    improve the way how search.json is generated

--- search_json.py
+++ search_json.py
@@ -15,32 +15,51 @@ def md5(data):
     return hashlib.md5(data).hexdigest()
 
 
-search_terms = ["hpv", "#hpv", "Human Papillomavirus Vaccines", "Human Papillomavirus Vaccine",
+
+
+
+def generate_search_json(query_terms, search_json_filename):
+
+    with open(search_json_filename, 'w') as wf:
+        results = {}
+
+        #querystring = '%s'%(', '.join('"' + term.lower() + '"' for term in query_terms))
+        querystring = ''
+
+        current_query_terms = []
+
+        for term in query_terms:
+            querystring = '%s OR %s'%(querystring, term.lower())
+            current_query_terms.append(term.lower())
+
+            if (len(querystring) > 200):
+
+                output_filename = md5(querystring.encode('utf-8'))
+
+                logger.info(current_query_terms)
+                
+                results[output_filename] = {
+                    "terms": current_query_terms,
+                    "since_id": 0,
+                    "querystring": querystring,
+                    "output_filename": output_filename
+                }
+
+                current_query_terms = []
+                querystring = ''
+
+
+        json.dump(results, wf)
+
+
+if __name__=="__main__":
+
+    search_terms = ["hpv", "#hpv", "Human Papillomavirus Vaccines", "Human Papillomavirus Vaccine",
     "HumanPapillomavirusVaccine", "#HumanPapillomavirusVaccine",
     "Gardasil", "#Gardasil", "Human Papillomavirus", "Cervarix",
     "cervical cancer", "cervical #cancer", "#cervical cancer", "cervicalcancer", "#cervicalcancer"
     ]
-def generate_search_json():
 
-    with open('search.json', 'w') as wf:
-        results = {}
+    query_terms = ['"%s"'%(term) for term in search_terms]
 
-        querystring = '%s'%(', '.join('"' + term.lower() + '"' for term in search_terms))
-
-        logger.info(querystring)
-        #quit()
-        output_filename = md5(querystring.encode('utf-8'))
-
-        
-        results[output_filename] = {
-            "terms": search_terms,
-            "since_id": 0,
-            "querystring": querystring,
-            "output_filename": output_filename
-        }
-
-        json.dump(results, wf)
-
-if __name__=="__main__":
-
-    generate_search_json()
+    generate_search_json(query_terms, "hpv_search.json")
